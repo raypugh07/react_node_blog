43d02a022cb2586bff2f9fd55479ef6eddfec529
Your Name <you@example.com>
2020-01-14 21:36:12 -0600

    setup context state,context_state_config.js

--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -1,1 +1,7 @@
-// holds the context object and will be imported to every componet that uses the useContext() hook
+// holds the context object and will be imported to every component that uses the useContext() hook
+
+import React from 'react';
+
+const Context = React.createContext()
+
+export default Context;
