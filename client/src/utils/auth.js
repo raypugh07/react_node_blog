081b8b9c918086b17ebcf5fd08c78aa95778aa7f
Your Name <you@example.com>
2020-01-20 23:51:49 -0600

    changed calledback uri,auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,7 +7,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: "dev-b7l7o2zo.auth0.com",
     clientID: 'ZSVujnyP6b1vNBhIvy3Laph4LTwScj9r',
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: 'https://warm-island-33082.herokuapp.com/callback',
     responseType: 'token id_token',
     scope: 'openid profile email'
   })
