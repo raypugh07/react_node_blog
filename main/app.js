c900db20895ad17719ec7e77e472a78bde895b3e
Your Name <you@example.com>
2020-01-20 23:00:20 -0600

    added react build code to server,app.js

--- main/app.js
+++ main/app.js
@@ -16,6 +16,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cors());
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 
 app.use('/', indexRouter)
 
